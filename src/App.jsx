01ba3acb3b641005487b40b8e69d78ee3c799ea1
Pomelodies <randomelodies@gmail.com>
2025-11-13 15:18:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  // 📌 ETABLIR LES STATES
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [reset, setReset] = useState(false);
+
+  return (
+    <>
+      <header>
+        <h1>Create account</h1>
+      </header>
+      <main>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div>
+            <label htmlFor="Name"> Name</label>
+            <input
+              type="text"
+              placeholder="Jean Dupont"
+              id="Name"
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              placeholder="jeandupont@lereacteur.io"
+              id="email"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              placeholder="lErEaCtEuR2020"
+              id="password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="confirmpassword">Confirm your Password</label>
+            <input
+              type="text"
+              placeholder="lErEaCtEuR2020"
+              id="confirmpassword"
+              value={confirmPassword}
+              onChange={(event) => {
+                setConfirmPassword(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button
+              className="register"
+              onClick={() => {
+                setReset(true);
+              }}
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </main>
+      <footer>
+        <p>
+          Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+          <span>Elodiiiiie</span> 🐸
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
